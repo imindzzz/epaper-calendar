be43e8b86b3636e0a6af2cd25416be85eee2c75d
FuSun <332553703@qq.com>
2021-04-27 15:41:21 +0800

    chore: 多个hello world

--- src/main.cpp
+++ src/main.cpp
@@ -85,9 +85,9 @@
 
 // somehow there should be an easier way to do this
 #define GxEPD2_BW_IS_GxEPD2_BW true
-#define GxEPD2_3C_IS_GxEPD2_3C true
-#define GxEPD2_7C_IS_GxEPD2_7C true
-#define GxEPD2_1248_IS_GxEPD2_1248 true
+#define GxEPD2_3C_IS_GxEPD2_3C false
+#define GxEPD2_7C_IS_GxEPD2_7C false
+#define GxEPD2_1248_IS_GxEPD2_1248 false
 #define IS_GxEPD(c, x) (c##x)
 #define IS_GxEPD2_BW(x) IS_GxEPD(GxEPD2_BW_IS_, x)
 #define IS_GxEPD2_3C(x) IS_GxEPD(GxEPD2_3C_IS_, x)
@@ -103,6 +103,7 @@
 #elif IS_GxEPD2_7C(GxEPD2_DISPLAY_CLASS)
 #define MAX_HEIGHT(EPD) (EPD::HEIGHT <= (MAX_DISPLAY_BUFFER_SIZE) / (EPD::WIDTH / 2) ? EPD::HEIGHT : (MAX_DISPLAY_BUFFER_SIZE) / (EPD::WIDTH / 2))
 #endif
+
 GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, MAX_HEIGHT(GxEPD2_DRIVER_CLASS)> display(GxEPD2_DRIVER_CLASS(/*CS=*/15, /*DC=*/27, /*RST=*/26, /*BUSY=*/25));
 #endif
 
@@ -152,6 +153,15 @@ void helloWorld()
   {
     display.fillScreen(GxEPD_WHITE);
     display.setCursor(x, y);
+    display.print(HelloWorld);
+
+    display.setCursor(x, y + tbh * 2);
+    display.print(HelloWorld);
+
+    display.setCursor(x, y + tbh *4);
+    display.print(HelloWorld);
+
+    display.setCursor(x, y + tbh * 6);
     display.print(HelloWorld);
   } while (display.nextPage());
   //Serial.println("helloWorld done");
